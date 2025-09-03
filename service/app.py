2148d26401561a7cf44ceccbb1c611b32f319e73
alvaranz83 <103906142+alvaranz83@users.noreply.github.com>
2025-09-03 02:58:47 +0200

    Update app.py

--- service/app.py
+++ service/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File, Form, Request
 from datetime import datetime, timezone
 from typing import Optional, List
-import os, json, textwrap, re, uuid, base64, logging
+import os, json, textwrap, re, uuid, base64, logging, io
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
