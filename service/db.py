71e586c4cecc18c3481fe12ccce311ba053029af
alvaranz83 <103906142+alvaranz83@users.noreply.github.com>
2025-09-28 17:42:40 +0200

    Update db.py

--- service/db.py
+++ service/db.py
@@ -2,11 +2,11 @@
 import os
 from databases import Database
 
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASS = os.getenv("DB_PASS", "yourpassword")
-DB_NAME = os.getenv("DB_NAME", "recruiting_api_dev")
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
